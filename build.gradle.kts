4bc0916310e346c985f096f505f2d3476b06ec90
aaditx23 <adit.pranto@gmail.com>
2025-04-24 11:49:21 +0600

    Feat: Add icon

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2024.1.7")
-    type.set("IU") // Target IDE Platform
+    type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(
         "java",
